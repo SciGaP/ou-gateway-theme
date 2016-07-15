a5812fd52edcec117ac3c5a5ca9e455c0aea405d
Nipurn Doshi <nipunhere@gmail.com>
2016-07-15 00:33:03 -0700

    Oscer Theme

---
+++ partials/template.blade.php
@@ -0,0 +1,42 @@
+<div class="col-md-12 hero-unit scigap-top">
+	<!--
+	<h1>Hero Image</h1>
+	<h2>This is a simple hero unit, a simple jumbotron-style component for calling extra 7
+	attention to featured content or information.</h2>
+	<p><a class="btn btn-primary btn-large">Learn more</a></p>
+	-->
+</div>
+
+<div class="col-md-offset-2 col-md-8 breathing-space seagrid-info">
+	<h1 class="text-center">OU Super Computing Center</h1>
+	<p class="text-center" style="color:#cccccc;">
+		The OU Supercomputing Center for Education & Research, a division of OU Information Technology, helps undergraduates, grad students, faculty and staff to learn and use advanced computing in their science and engineering research and education.<br/>
+	</p>
+	<hr/>
+	<div class="col-md-5">
+	<p style="font-size:14px; margin-top:20px;">
+		The project is led by Information Technology and includes faculty and staff in the College of Engineering, the School of Computer Science, the School of Electrical & Computer Engineering, the School of Meteorology, the Homer L. Dodge Department of Physics & Astronomy, the Department of Microbiology & Plant Biology, the Center for Analysis & Prediction of Storms, the Cooperative Institute for Mesoscale Meteorological Studies, the Center for Spatial Analysis, and the Earth Observation Modeling Facility. 
+	</p>
+	</div>
+	<div class="col-md-offset-1 col-md-5">
+	<p style="font-size:14px; margin-top:20px;">
+	Currently, OSCER hardware resources are housed in the Stephenson Research & Technology Center and Merrick Computing Center, both on the OU Norman campus.</p>
+	<p>OSCER is a member of the <a href="http://casc.org/" target="_blank">Coalition for Academic Scientific Computation.</a></p>
+	</div>
+
+</div>
+
+
+<div class="col-md-offset-2 col-md-8 text-center" style="margin-top:50px;">
+<h3>Create your Account Today!</h3>
+<a href="{{ URL::to('/') }}/create">
+<button class="btn btn-primary btn-lg" style="border-radius:0; background-color:#900C3F; border:0; margin-top:10px;">Create Account</button></a>
+</div>
+
+<!--
+<div class="col-md-12 text-center blue-bg" style="padding:2.5%">
+	<a href="https://seagrid.org/jnlp/seagrid.jnlp">
+	<button style="padding:2.5%;" class="btn btn-primary col-md-offset-4 col-md-4">Download Desktop App</button>
+	</a>
+</div>
+-->
